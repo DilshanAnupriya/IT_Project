b1b0a28afed27472d98d7113cc6ecf4ef278c317
dilshan anupriya <dilshananupriya@icloud.com>
2024-08-18 04:27:13 +0530

    Create folder structure
    Create Backend folder , frontend folder and connected to mongoDB

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+
+//password B03_07
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//Middleware
+app.use("/", (req, res, next) => {
+    res.send("It is Working");
+})
+
+mongoose.connect("mongodb+srv://Admin:B03_07@cluster0.3giug.mongodb.net/")
+    .then(() => console.log("Connected to Mongodb"))
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch((err) => console.log((err)));
