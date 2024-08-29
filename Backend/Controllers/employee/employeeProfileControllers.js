31852be889a85e4d00d52667218e77de3e1fe4ef
methsu <thiyaana.vidanaarachchi@gmail.com>
2024-08-30 04:19:37 +0530

    employee profile done
    employee profile model, controllers, routes and server.js all are done

---
+++ Backend/Controllers/employee/employeeProfileControllers.js
@@ -0,0 +1,123 @@
+const { get } = require('mongoose');
+const Profile = require("../../Model/employee/employeeProfileModel");
+
+//display task
+const getAllProfile = async (req, res, next) => {
+    let emp;
+
+    //get all Profile
+    try {
+        emp = await Profile.find();
+    } catch (error) {
+        console.log(error);
+    }
+
+    //not found 
+    if(!emp){
+        return res.status(404).jason({ massage: "unable to display" })
+    }
+
+    //display profile
+    return res.ststus(200).jason({ emp });
+}
+
+
+//add Profile
+const createProfile = async (req, res, next) => {
+    const {first_name, last_name, job_role, nic, email, qualifications, bank_details, joined_date} = req.body;
+
+    let emp;
+
+    //create Profile
+    try {
+        emp = new Profile({ first_name, last_name, job_role, nic, email, qualifications, bank_details, joined_date });
+        await emp.save(); //profile or emp
+    } catch (error) {
+        console.log(error);
+    }
+
+    //not found 
+    if(!emp){
+        return res.status(404).jason({ massage: "unable to Create" })
+    }
+
+    //create profile
+    return res.ststus(200).jason({ emp });
+}
+
+
+//get by id 
+const getIdByProfile = async (req, res, next) => {
+    let id = req.params.id;
+    let emp;
+
+    //get profile by id
+    try {
+        emp = await Profile.findById(id);
+    } catch (error) {
+        console.log(error);
+    }
+
+    //not found 
+    if(!emp){
+        return res.status(404).jason({ massage: "unable to find" })
+    }
+
+    //find profile by id
+    return res.ststus(200).jason({ emp });
+}
+
+
+//update Profile
+const updateProfile = async (req, res, next) => {
+    let id = req.params.id;
+    let emp;
+    const {first_name, last_name, job_role, nic, email, qualifications, bank_details, joined_date} = req.body;
+
+    //update Profile
+    try {
+        emp = await Profile.findByIdAndUpdate(id, 
+            { first_name: first_name, last_name: last_name, job_role: job_role, nic: nic, email: email, qualifications: qualifications, bank_details: bank_details, joined_date: joined_date }
+        );
+        emp = await emp.save(); //profile or emp
+    } catch (error) {
+        console.log(error);
+    }
+
+    //not found 
+    if(!emp){
+        return res.status(404).jason({ massage: "unable to update" })
+    }
+
+    //update profile
+    return res.ststus(200).jason({ emp });
+}
+
+
+//delete profile 
+const deleteProfile = async (req, res, next) => {
+    let id = req.params.id;
+    let emp;
+
+    //get profile by id
+    try {
+        emp = await Profile.findByIdAndDelete(id);
+    } catch (error) {
+        console.log(error);
+    }
+
+    //not found 
+    if(!emp){
+        return res.status(404).jason({ massage: "unable to find" })
+    }
+
+    //find profile by id
+    return res.ststus(200).jason({ emp });
+}
+
+//export
+exports.getAllProfile = getAllProfile
+exports.createProfile = createProfile
+exports.getIdByProfile = getIdByProfile
+exports.updateProfile = updateProfile
+exports.deleteProfile = deleteProfile
--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./Routes/Volunteers/VolunteerRoutes");
 const task = require("./Routes/Volunteers/VolunteerTaskRouts");
-
+ //emp profile
+ const profile = require("./Routes/employee/employeeProfileRoutes");
 
 
 const app = express();
@@ -17,6 +18,8 @@ app.use(express.json());
 app.use(cors());
 app.use("/users", router);
 app.use("/task", task);
+//emp profile
+app.use("/employees", profile);
 
 mongoose.connect("mongodb+srv://Admin:B03_07@cluster0.3giug.mongodb.net/")
     .then(() => console.log("Connected to Mongodb"))
