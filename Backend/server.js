1a27fab4e466acc037b198722d9f9c04edcdf172
dilshan anupriya <dilshananupriya@icloud.com>
2024-09-19 01:09:01 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -39,7 +39,7 @@ app.use("/employees", profile);
 app.use("/Availability", Availability);
 app.use("/requests", Requests);
 
-<<<<<<< Updated upstream
+
 
 
 //medpatient 
@@ -53,8 +53,7 @@ app.use("/Dinner", Dinner);
 
 
 
-=======
->>>>>>> Stashed changes
+
 mongoose.connect("mongodb+srv://Admin:B03_07@cluster0.3giug.mongodb.net/")
     .then(() => console.log("Connected to Mongodb"))
     .then(() => {
