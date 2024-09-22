41dacb885d300d313cc631b56aaabd2dde77e231
dilshan anupriya <dilshananupriya@icloud.com>
2024-09-22 22:45:08 +0530

    create dashboard

---
+++ frontend/src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import "../../Pages/Css/ComponentsCss/Dashboard/Dashboard.css"
+import logo from "../../Assets/logo.png"
+import { Link } from 'react-router-dom'
+function Dashboard() {
+
+
+
+
+    return (
+        <div>
+              <div className="container11">
+        <div className="navigation">
+            <ul>
+                <li>
+                    <Link to="">
+                        <span className="logo">
+                            <img src={logo} alt='logo'/>
+                        </span>
+                        <span className="title5">Care Zone</span>
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="">
+                        <span className="icon">
+                        <i className="fa fa-home fa-2x" aria-hidden="true"></i>
+                          
+                        </span>
+                        <span className="title">Dashboard</span>
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="">
+                        <span className="icon">
+                            <i className="fa fa-user fa-2x" aria-hidden="true"></i>
+                        </span>
+                        <span className="title">Profile</span>
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="">
+                        <span className="icon">
+                            <i className="fa fa-desktop fa-2x" aria-hidden="true"></i>
+                        </span>
+                        <span className="title">Order</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="">
+                        <span className="icon">
+                            <i className="fa fa-desktop fa-2x" aria-hidden="true"></i>
+                        </span>
+                        <span className="title">Settings</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="">
+                        <span className="icon">
+                            <i className="fa fa-desktop fa-2x" aria-hidden="true"></i>
+                        </span>
+                        <span className="title">Log Out</span>
+                    </Link>
+                </li>
+
+
+            </ul>
+        </div>
+
+ 
+        <div className="main">
+            <div className="topbar">
+                <div className="toggle">
+                   <i className="fa fa-bars fa-1x" aria-hidden="true"></i>
+
+                </div>
+
+                <div className="search">
+                    <label>
+                        <input type="text" placeholder="Search here"/>
+                        <ion-icon name="search-outline"></ion-icon>
+                    </label>
+                </div>
+
+                <div className="user">
+                    <img src="" alt=""/>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+    )
+}
+
+export default Dashboard
