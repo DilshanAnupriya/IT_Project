70975238541c69be98ccf15f1642374aa3df12f6
madushan321 <IT22182432@my.sliit.lk>
2024-09-23 00:29:31 +0530

    update userRoutes
    no errors

--- Backend/Routes/user/userRoutes.js
+++ Backend/Routes/user/userRoutes.js
@@ -5,7 +5,7 @@ const User =  require("../../Controllers/user/userControllers");
 router.get('/all', User.getAllUsers);
 router.post('/add', User.addUser);
 router.get('/:id', User.getById);
-router.patch('/:id', User.updateUser);
+router.put('/:id', User.updateUser);
 router.delete('/:id', User.deleteUser);
 
 module.exports = router;
