15e8729ab16e78074281839161c582f2b29ca31d
methsu <thiyaana.vidanaarachchi@gmail.com>
2024-09-23 22:23:29 +0530

    main Dashboard
    employee main dashboard frontend and display

---
+++ frontend/src/Pages/Employee/EmpDashBoard.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import "../Css/Employee/EmpDashboard.css";
+import Dash from "../../Components/Dashboard/Dashboard";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Searchpng from "../../Assets/Employee/search.png"
+
+const URL = "http://localhost:3000/employees/";
+
+const fetchHandler = async () => {
+    const data = await axios.get(URL).then((res) => res.data);
+    console.log(data);
+    return data;
+};
+
+function EmpDashBoard() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetchHandler().then((data) => setUsers(data.emp));
+        console.log(users);
+    }, []);
+
+    return (
+        <div>
+            <div className='dashboard36'>
+                <Dash />
+            </div>
+            <main className="table36" id="customers_table">
+                <section className="table__header36">
+                    <h1>Employee Details</h1>
+                    <div className="input-group36">
+                        <input type="search" placeholder="Search Data..." />
+                        <img src={Searchpng} alt="" />
+                    </div>
+                </section>
+                <section className="table__body36">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Job Role</th>
+                                <th>NIC</th>
+                                <th>Email</th>
+                                <th>Qualifications</th>
+                                <th>Bank Details</th>
+                                <th>Join Date</th>
+                                <th>Other</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map((user) => (
+                                <tr key={user._id}>
+                                    <td>{user._id}</td>
+                                    <td>{user.first_name}</td>
+                                    <td>{user.last_name}</td>
+                                    <td>{user.job_role}</td>
+                                    <td>{user.nic}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.qualifications}</td>
+                                    <td>{user.bank_details}</td>
+                                    <td>{new Date(user.joined_date).toLocaleDateString()}</td>
+                                    <td>
+                                        <div className='action36'>
+                                            <Link to="/#"><button className='edit36'>Edit</button></Link>
+                                            <button className='del36'>Delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </section>
+            </main>
+        </div>
+    );
+}
+
+export default EmpDashBoard;
--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Nav from "../../Components/Navbar/Navbar"
 import Footer from '../../Components/Footer/Footer'
 import Dash from "../../Components/Dashboard/Dashboard"
-
+import EDash from "../Employee/EmpDashBoard"
 
 
 function Home() {
@@ -11,8 +11,7 @@ function Home() {
 
     return (
         <div >
-           <Nav/>
-           <Footer/>
+           <EDash/>
         </div>
                 
                 )
