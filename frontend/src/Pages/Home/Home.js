f7c4a215f27d37b7d855af736e910a60571afccb
methsu <thiyaana.vidanaarachchi@gmail.com>
2024-09-23 23:38:11 +0530

    new new

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Nav from "../../Components/Navbar/Navbar"
 import Footer from '../../Components/Footer/Footer'
-import Emform from "../Employee/EmpForm"
 
 function Home() {
     return (
         <div>
-            <Emform/>
+           
 
         </div>
     )
