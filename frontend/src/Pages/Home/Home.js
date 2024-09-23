66a94f6e1e698593f8208b3fc3bc1da6247068c3
dilshan anupriya <dilshananupriya@icloud.com>
2024-09-23 15:32:54 +0530

    home

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -2,19 +2,112 @@ import React from 'react'
 import Nav from "../../Components/Navbar/Navbar"
 import Footer from '../../Components/Footer/Footer'
 import Dash from "../../Components/Dashboard/Dashboard"
-
-
-
+import "../Home/Home.css"
+import pic from "../../Assets/d4.jpg"
+import { Link } from 'react-router-dom'
 function Home() {
-    
+
 
 
     return (
         <div >
-           <Nav/>
-           <Footer/>
+            <Nav />
+
+
+            <div className='d1'>
+                <img src={pic} alt='' />
+                <div className='s1'>
+                    <h1>Welcome to <br />Care Zone</h1>
+                    <h3>smkdcmdkmmfkmnjncjdnjjdn<br />ksdjkjknjcnd jdcnjcn</h3>
+                </div>
+
+                <div className='d2'>
+                    <img src={pic} alt='' />
+                    <div className='dis'>
+                        <p>About Us</p>
+                        <h1>Over 25 years of  Experience in this Business</h1>
+                        <p className='p1'>world best elder care .we provide best service to our elders and well expirence care givers world best elder care .we provide best service to our elders and well expirence care givers</p>
+                        <p className='p2'>world best elder care .we provide best service to our elders and well expirence care givers world best elder care .we provide best service to our elders and well expirence care givers</p>
+                        <div className='dflex'>
+                            <div className='s2'>
+                                <span>50+</span><br />
+                                <span>Care Givers</span>
+                            </div>
+                            <div className='s2'>
+                                <span>50+</span><br />
+                                <span>Elders</span>
+                            </div>
+                        </div>
+                        <button>See More</button>
+                    </div>
+                </div>
+
+                <div className='d3'>
+                    <div className='flex'>
+
+                        <div class="container90">
+
+                            <h1 class="heading90"><span>Our</span> Services</h1>
+
+                            <div class="box-container90">
+
+                                <div class="box90">
+                                    <img src="" alt="" />
+                                    <h3>Task </h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, commodi?</p>
+                                    <Link to="#" class="btn">View</Link>
+                                </div>
+
+                                <div class="box90">
+                                    <img src="" alt="" />
+                                    <h3>Certificate</h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, commodi?</p>
+                                    <Link to="#" class="btn">View </Link>
+                                </div>
+
+                                <div class="box90">
+                                    <img src="" alt="" />
+                                    <h3>Schedule</h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, commodi?</p>
+                                    <Link to="#" class="btn">View </Link>
+                                </div>
+
+                                <div class="box90">
+                                    <img src="" alt="" />
+                                    <h3>Task </h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, commodi?</p>
+                                    <Link to="#" class="btn">View</Link>
+                                </div>
+
+                                <div class="box90">
+                                    <img src="" alt="" />
+                                    <h3>Certificate</h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, commodi?</p>
+                                    <Link to="#" class="btn">View </Link>
+                                </div>
+
+                                <div class="box90">
+                                    <img src="" alt="" />
+                                    <h3>Schedule</h3>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, commodi?</p>
+                                    <Link to="#" class="btn">View </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='ft0'>
+                <Footer/>
+                </div>
+
+
+
+
+
+
         </div>
-                
-                )
+
+    )
 }
- export default Home
+export default Home
