47599b73e9671e178cc673fee6c49868a8d5e5ea
methsu <thiyaana.vidanaarachchi@gmail.com>
2024-09-24 03:44:10 +0530

    new new

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Nav from "../../Components/Navbar/Navbar"
 import Footer from '../../Components/Footer/Footer'
 import Dash from "../../Components/Dashboard/Dashboard"
-import EDash from "../Employee/EmpDashBoard"
 
 
 function Home() {
@@ -11,7 +10,6 @@ function Home() {
 
     return (
         <div >
-           <EDash/>
         </div>
                 
                 )
