c9c572cb4383761311576301a8ef5ff4ac97ac08
dilshan anupriya <dilshananupriya@icloud.com>
2024-09-23 18:14:18 +0530

    create display part completed

--- frontend/src/Pages/Volunteers/VolunteerProfileDash.js
+++ frontend/src/Pages/Volunteers/VolunteerProfileDash.js
@@ -1,136 +1,105 @@
-import React, { useEffect,useState } from 'react'
-import "../Css/Volunteers/VolunteerProfileDash.css"
-import Dash from "../../Components/Dashboard/Dashboard"
- import axios from "axios"
+import React, { useEffect, useState } from 'react';
+import "../Css/Volunteers/VolunteerProfileDash.css";
+import Dash from "../../Components/Dashboard/Dashboard";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
+const URL = "http://localhost:8000/users/";
 
+const fetchHandler = async () => {
+    const data = await axios.get(URL).then((res) => res.data);
+    console.log(data);
+    return data;
+};
 
-const URL ="http://localhost:3000/users/"
-
-
-const fetchHandler = async() =>{
-    return await axios.get(URL).then((res) => res.data);
-}
- 
 function VolunteerProfileDash() {
-   
-     const [users, setUsers] = useState();
-     useEffect(() =>{
-        fetchHandler().then((data) => setUsers(data.users));
-     },[])
+    const [users, setUsers] = useState([]);
 
-  return (
-    <div >
-            
-    <div className='dashboard'>
-   <Dash/>
-   </div>
-    <main class="table" id="customers_table">
+    useEffect(() => {
+        fetchHandler().then((data) => setUsers(data.Volunteers));
+        console.log(users);
+    }, []);
 
-        <section class="table__header">
-            <h1>Volunteer Details</h1>
-
-            <div class="input-group">
-                <input type="search" placeholder="Search Data..."/>
-                    <img src="images/search.png" alt=""/>
-             
-             </div>
-                    <div class="export__file">
-                        <label for="export-file" class="export__file-btn" title="Export File">+</label>
-                        <input type="checkbox" id="export-file"/>
-                            <div class="export__file-options">
-                                <label>Export As </label>
-                                <label for="export-file" id="toPDF">PDF <img src="images/pdf.png" alt=""/></label>
-                                <label for="export-file" id="toJSON">JSON <img src="images/json.png" alt=""/></label>
-                                <label for="export-file" id="toCSV">CSV <img src="images/csv.png" alt=""/></label>
-                                <label for="export-file" id="toEXCEL">EXCEL <img src="images/excel.png" alt=""/></label>
-                            </div>
+    return (
+        <div>
+            <div className='dashboard'>
+                <Dash />
+            </div>
+            <main className="table" id="customers_table">
+                <section className="table__header">
+                    <h1>Volunteer Details</h1>
+                    <div className="input-group">
+                        <input type="search" placeholder="Search Data..." />
+                        <img src="images/search.png" alt="" />
                     </div>
-        </section>
-
-                <section class="table__body">
+                    <div className="export__file">
+                        <label htmlFor="export-file" className="export__file-btn" title="Export File">+</label>
+                        <input type="checkbox" id="export-file" />
+                        <div className="export__file-options">
+                            <label>Export As </label>
+                            <label htmlFor="export-file" id="toPDF">PDF <img src="images/pdf.png" alt="" /></label>
+                            <label htmlFor="export-file" id="toJSON">JSON <img src="images/json.png" alt="" /></label>
+                            <label htmlFor="export-file" id="toCSV">CSV <img src="images/csv.png" alt="" /></label>
+                            <label htmlFor="export-file" id="toEXCEL">EXCEL <img src="images/excel.png" alt="" /></label>
+                        </div>
+                    </div>
+                </section>
+                <section className="table__body">
                     <table>
                         <thead>
                             <tr>
-                                <th> Id </th>
-                                <th> First Name </th>
-                                <th> Last Name </th>
-                                <th>  Date of Birth</th>
-                                <th> Gender </th>
-                                <th> Email </th>
-                                <th> Mobile </th>
-                                <th> Address </th>
-                                <th> Join Date </th>
-                                <th> Skills </th>
-                                <th> Duration</th>
-                                <th> Work</th>
-                                <th> Experience</th>
-                                <th> Days</th>
-                                <th> Time</th>
-                                <th> Description</th>
-                                <th> Action</th>
+                                <th>Id</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Date of Birth</th>
+                                <th>Gender</th>
+                                <th>Email</th>
+                                <th>Mobile</th>
+                                <th>Address</th>
+                                <th>Join Date</th>
+                                <th>Skills</th>
+                                <th>Duration</th>
+                                <th>Work</th>
+                                <th>Experience</th>
+                                <th>Days</th>
+                                <th>Time</th>
+                                <th>Description</th>
+                                <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td> 1 </td>
-                                <td> Dilshan</td>
-                                <td> Anupriya </td>
-                                <td> 2002-11-26 </td>
-                                <td> Male </td>
-                                <td> dilshananupriya@gmail.com</td>
-                                <td> 0703234653 </td>
-                                <td> 77/46 hokandara </td>
-                                <td> Technical </td>
-                                <td> Yes </td>
-                                <td> 1-Month </td>
-                                <td> companionship</td>
-                                <td> Evening </td>
-                                <td>WD</td>
-                                <td>Evening</td>
-                                <td>for job</td>
-                                <td> 
-                                    <div className='action'>
-                                    <button>up</button>
-                                    <button className='del'>del</button>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td> 1 </td>
-                                <td> Dilshan</td>
-                                <td> Anupriya </td>
-                                <td> 2002-11-26 </td>
-                                <td> Male </td>
-                                <td> dilshananupriya@gmail.com</td>
-                                <td> 0703234653 </td>
-                                <td> 77/46 hokandara </td>
-                                <td> Technical </td>
-                                <td> Yes </td>
-                                <td> 1-Month </td>
-                                <td> companionship</td>
-                                <td> Evening </td>
-                                <td>WD</td>
-                                <td>Evening</td>
-                                <td>for job</td>
-                                <td> 
-                                    <div className='action'>
-                                    <button>up</button>
-                                    <button className='del'>del</button>
-                                    </div>
-                                </td>
-                            </tr>
-                           
+                            {users.map((user) => (
+                                <tr key={user._id}>
+                                    <td>{user._id}</td>
+                                    <td>{user.first_name}</td>
+                                    <td>{user.last_name}</td>
+                                    <td>{new Date(user.date_of_birth).toLocaleDateString()}</td>
+                                    <td>{user.gender}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.mobile}</td>
+                                    <td>{user.address}</td>
+                                    <td>{new Date(user.date).toLocaleDateString()}</td>
+                                    <td>{user.skills}</td>
+                                    <td>{user.duration}</td>
+                                    <td>{user.type_of_work}</td>
+                                    <td>{user.experience}</td>
+                                    <td>{user.days}</td>
+                                    <td>{user.time}</td>
+                                    <td>{user.description}</td>
+                                    <td>
+                                        <div className='action'>
+                                            <Link to="/volunteer_pd_update"><button>up</button></Link>
+                                            <button className='del'>del</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </section>
             </main>
-
-            <div className='new'>
-                <h1>hello</h1>
-            </div>
         </div>
-  )
+    );
 }
 
-export default VolunteerProfileDash
+export default VolunteerProfileDash;
