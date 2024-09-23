4c812f352a823ed06e266ccc34f5ef8f090e24ff
methsu <thiyaana.vidanaarachchi@gmail.com>
2024-09-24 04:54:18 +0530

    new
    new

--- frontend/src/Pages/Employee/EmpDashBoard.js
+++ frontend/src/Pages/Employee/EmpDashBoard.js
@@ -3,7 +3,7 @@ import "../Css/Employee/EmpDashboard.css";
 import Dash from "../../Components/Dashboard/Dashboard";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import Searchpng from "../../Assets/Employee/search.png"
+import Searchpng from "../../Assets/Employee/search.png";
 
 const URL = "http://localhost:3000/employees/";
 
@@ -15,10 +15,31 @@ const fetchHandler = async () => {
 
 function EmpDashBoard() {
     const [users, setUsers] = useState([]);
+    const [counts, setCounts] = useState({
+        careGivers: 0,
+        doctors: 0,
+        nurses: 0,
+        nutritionists: 0
+    });
 
     useEffect(() => {
-        fetchHandler().then((data) => setUsers(data.emp));
-        console.log(users);
+        fetchHandler().then((data) => {
+            setUsers(data.emp);
+
+            // Calculating counts based on job role
+            const careGiverCount = data.emp.filter(user => user.job_role === 'Care Giver').length;
+            const doctorCount = data.emp.filter(user => user.job_role === 'Doctor').length;
+            const nurseCount = data.emp.filter(user => user.job_role === 'Nurse').length;
+            const nutritionistCount = data.emp.filter(user => user.job_role === 'Nutritionist').length;
+
+            // Setting the counts in the state
+            setCounts({
+                careGivers: careGiverCount,
+                doctors: doctorCount,
+                nurses: nurseCount,
+                nutritionists: nutritionistCount
+            });
+        });
     }, []);
 
     return (
@@ -26,25 +47,26 @@ function EmpDashBoard() {
             <div className='dashboard70'>
                 <Dash />
             </div>
-            
+
+            {/* Dynamic Count Boxes */}
             <section className="count-panel">
-    <div className="count-box">
-        <h2>Care Givers</h2>
-        <p>Number of Care givers: 16</p>
-    </div>
-    <div className="count-box">
-        <h2>Doctors</h2>
-        <p>Number of Doctors: 9</p>
-    </div>
-    <div className="count-box">
-        <h2>Nurses</h2>
-        <p>Number of Nurses: 12</p>
-    </div>
-    <div className="count-box">
-        <h2>Other</h2>
-        <p>Number of Other Staff: 7</p>
-    </div>
-</section>
+                <div className="count-box">
+                    <h2>Care Givers</h2>
+                    <p>Number of Care givers: {counts.careGivers}</p>
+                </div>
+                <div className="count-box">
+                    <h2>Doctors</h2>
+                    <p>Number of Doctors: {counts.doctors}</p>
+                </div>
+                <div className="count-box">
+                    <h2>Nurses</h2>
+                    <p>Number of Nurses: {counts.nurses}</p>
+                </div>
+                <div className="count-box">
+                    <h2>Nutritionists</h2>
+                    <p>Number of Nutritionists: {counts.nutritionists}</p>
+                </div>
+            </section>
 
             <main className="table36" id="customers_table">
                 <section className="table__header36">
@@ -53,7 +75,7 @@ function EmpDashBoard() {
                         <input type="search" placeholder="Search Data..." />
                         <img src={Searchpng} alt="" />
                     </div>
-                    
+
                     <button className='add36'>Add Employee</button>
                 </section>
                 <section className="table__body36">
@@ -86,8 +108,8 @@ function EmpDashBoard() {
                                     <td>{new Date(user.joined_date).toLocaleDateString()}</td>
                                     <td>
                                         <div className='action36'>
-                                            <Link to="/#"><button className='edit36'>Edit</button></Link>
-                                            <button className='del36'>Delete</button>
+                                            <Link to="/#"><button className='edit36'>Edit   ✏</button></Link>
+                                            <button className='del36'>delete🗑</button>
                                         </div>
                                     </td>
                                 </tr>
