f9da29f8a147ccc83de73c4b89bcb94b8ad6bf40
NaveenGunasekara <naveengunasekara62@gmail.com>
2024-09-24 11:19:43 +0530

    wrong

---
+++ frontend/src/Pages/ClubandSociety/Clubandsociety.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import "../../Pages/Css/clubandsociety/clubandsociety";
+import { useNavigate } from 'react-router-dom';
+import Nav from  "../../Components/Navbar/Navbar"
+import Footer from "../../Components/Footer/Footer"
+
