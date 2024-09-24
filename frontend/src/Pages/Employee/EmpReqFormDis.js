2461635b1a9a6187a4611fa718127946326758d5
methsu <thiyaana.vidanaarachchi@gmail.com>
2024-09-24 08:21:50 +0530

    request form display
    done display

--- frontend/src/Pages/Employee/EmpReqFormDis.js
+++ frontend/src/Pages/Employee/EmpReqFormDis.js
@@ -16,7 +16,7 @@ function VolunteerProfileDash() {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        fetchHandler().then((data) => setUsers(data.request));
+        fetchHandler().then((data) => setUsers(data.requ));
         console.log(users);
     }, []);
 
@@ -40,15 +40,16 @@ function VolunteerProfileDash() {
                             <tr>
                                 <th>Id</th>
                                 <th>Name</th>
-                                <th>Last Name</th>
-                                <th>Date of Birth</th>
-                                <th>Gender</th>
                                 <th>Email</th>
-                                <th>Mobile</th>
+                                <th>Phone no</th>
+                                <th>Gender</th>
+
                                 <th>Address</th>
-                                <th>Join Date</th>
-                                <th>Skills</th>
-                                <th>Duration</th>
+                                <th>Education Qualification</th>
+                                <th>Experience</th>
+                                <th>Qualification years</th>
+                                <th>Computer Literacy</th>
+                                <th>English Skill</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
@@ -56,21 +57,21 @@ function VolunteerProfileDash() {
                             {users.map((user) => (
                                 <tr key={user._id}>
                                     <td>{user._id}</td>
-                                    <td>{user.first_name}</td>
-                                    <td>{user.last_name}</td>
-                                    <td>{new Date(user.date_of_birth).toLocaleDateString()}</td>
-                                    <td>{user.gender}</td>
+                                    <td>{user.name}</td>
                                     <td>{user.email}</td>
-                                    <td>{user.mobile}</td>
+                                    <td>{user.phone_no}</td>
+                                    <td>{user.gender}</td>
+                                    
                                     <td>{user.address}</td>
-                                    <td>{new Date(user.date).toLocaleDateString()}</td>
-                                    <td>{user.skills}</td>
-                                    <td>{user.duration}</td>
-                                    <td>{user.description}</td>
+                                    <td>{user.edu_qualifications}</td>
+                                    <td>{user.experience}</td>
+                                    <td>{user.yearsofQualification}</td>
+                                    <td>{user.computerLiteracy}</td>
+                                    <td>{user.englishSkills}</td>
                                     <td>
                                         <div className='action'>
-                                            <Link to="/volunteer_pd_update"><button>up</button></Link>
-                                            <button className='del'>del</button>
+                                            <Link to="/#"><button className='edit36'>Edit</button></Link>
+                                            <button className='del36'>del</button>
                                         </div>
                                     </td>
                                 </tr>
