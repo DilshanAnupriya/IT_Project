5b1a61239359982279fc9ec0e17cb9948e0b3a3a
KSD <it22158840@my.sliit.lk>
2024-10-07 22:06:59 +0530

    ddd

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -1,5 +1,5 @@
 import { FaHome, FaUser, FaChartBar, FaCog, FaSignOutAlt } from "react-icons/fa";
-import { MdDashboard, MdWidgets } from "react-icons/md"; 
+import { MdDashboard, MdWidgets } from "react-icons/md";
 import { IoToggle } from "react-icons/io5";
 import Logo from "../Assets/logo_web.png";
 import { Link } from "react-router-dom";
@@ -7,24 +7,35 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
     <div className="h-screen w-16 bg-white flex flex-col items-center py-4 space-y-8 shadow-lg">
-      
+
       <div className="space-y-8">
 
-      <div className="j">
+        <div className="j">
           <img src={Logo} className="k" alt="logo" />
         </div>
 
-        <MdDashboard className="text-blue-600 hover:text-blue-800 cursor-pointer" size={24} />
+        {/* <MdDashboard className="text-blue-600 hover:text-blue-800 cursor-pointer" size={24} /> */}
       </div>
 
-  
+
       <div className="space-y-8">
-  
-        <FaUser className="text-black hover:text-blue-600 cursor-pointer" size={24} />
+        <Link to="/admin-review">
+          <FaUser className="text-black hover:text-blue-600 cursor-pointer" size={24} />
+        </Link>
 
-        <FaChartBar className="text-black hover:text-blue-600 cursor-pointer" size={24} />
+        
+          <FaUser className="text-black hover:text-blue-600 cursor-pointer" size={24} />
+       
 
+
+        <Link to="/report">
+          <FaChartBar className="text-black hover:text-blue-600 cursor-pointer"
+            size={24} />
+        </Link>
+
+        <Link to="/admin-complaint">
         <IoToggle className="text-black hover:text-blue-600 cursor-pointer" size={24} />
+        </Link>
       </div>
 
       <div className="space-y-8 mt-auto">
