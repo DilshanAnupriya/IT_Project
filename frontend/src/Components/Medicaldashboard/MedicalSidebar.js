ad41a385edf36a6ffd8f6b59b655da4bd29c1fba
LEXES7 <sachinthabhashitha245@gmail.com>
2024-10-13 21:44:19 +0530

    changes

--- frontend/src/Components/Medicaldashboard/MedicalSidebar.js
+++ frontend/src/Components/Medicaldashboard/MedicalSidebar.js
@@ -14,7 +14,7 @@ const MedicalDashboard = () => {
             <aside id="medical-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
                 <div className="h-full flex flex-col justify-between px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
                     <div>
-                        <a href="https://flowbite.com/" className="flex items-center ps-2.5 mb-10"> {/* Increased bottom margin */}
+                        <a href="/" className="flex items-center ps-2.5 mb-10"> {/* Increased bottom margin */}
                             <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 me-3 sm:h-7" alt="Flowbite Logo" />
                             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Care Zone</span>
                         </a>
